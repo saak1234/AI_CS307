ee93474ba4eefa743cd59af92545f73beedcc1c1
Akshoy <akshoygorai33@gmail.com>
2024-09-29 15:03:48 +0530

    updated Week1

--- Week1/Week1_B.py
+++ Week1/Week1_B.py
@@ -1,84 +1,117 @@
 from collections import deque
 
-class State:
-    def __init__(self, left_m, left_c, right_m, right_c, boat):
-        self.left_m = left_m
-        self.left_c = left_c
-        self.right_m = right_m
-        self.right_c = right_c
-        self.boat = boat
-
-    def is_valid(self):
-        if self.left_m < 0 or self.left_c < 0 or self.right_m < 0 or self.right_c < 0:
-            return False
-        if (self.left_m > 0 and self.left_m < self.left_c) or (self.right_m > 0 and self.right_m < self.right_c):
-            return False
-        return True
-
-    def is_goal(self):
-        return self.left_m == 0 and self.left_c == 0
-
-    def __hash__(self):
-        return hash((self.left_m, self.left_c, self.right_m, self.right_c, self.boat))
-
-    def __eq__(self, other):
-        return (self.left_m, self.left_c, self.right_m, self.right_c, self.boat) == (other.left_m, other.left_c, other.right_m, other.right_c, other.boat)
-
-    def __str__(self):
-        return f'Left: M:{self.left_m}, C:{self.left_c} | Right: M:{self.right_m}, C:{self.right_c} | Boat: {"Left" if self.boat == 1 else "Right"}'
-
-def get_successors(state):
-    moves = [(1, 0), (2, 0), (0, 1), (0, 2), (1, 1)]
-    successors = []
-    for m, c in moves:
-        if state.boat == 1:
-            new_state = State(state.left_m - m, state.left_c - c, state.right_m + m, state.right_c + c, 0)
-        else:
-            new_state = State(state.left_m + m, state.left_c + c, state.right_m - m, state.right_c - c, 1)
-        if new_state.is_valid():
-            successors.append(new_state)
-    return successors
-
-def bfs():
-    start = State(3, 3, 0, 0, 1)
-    queue = deque([(start, [])])
-    explored = set()
+# BFS for Rabbit Leap
+def bfs_rabbit_leap(initial_state, goal_state):
+    queue = deque([(initial_state, [])])
+    visited = set([tuple(initial_state)])
+    
     while queue:
         current_state, path = queue.popleft()
-        if current_state.is_goal():
+
+        if current_state == goal_state:
             return path + [current_state]
-        explored.add(current_state)
-        for successor in get_successors(current_state):
-            if successor not in explored and successor not in (s[0] for s in queue):
-                queue.append((successor, path + [current_state]))
+
+        for i in range(len(current_state)):
+            if current_state[i] == '.':
+                if i > 0 and current_state[i - 1] != '.':
+                    new_state = current_state[:]
+                    new_state[i], new_state[i - 1] = new_state[i - 1], new_state[i]
+                    if tuple(new_state) not in visited:
+                        visited.add(tuple(new_state))
+                        queue.append((new_state, path + [current_state]))
+
+                if i > 1 and current_state[i - 1] != '.' and current_state[i - 2] != '.':
+                    new_state = current_state[:]
+                    new_state[i], new_state[i - 2] = new_state[i - 2], new_state[i]
+                    if tuple(new_state) not in visited:
+                        visited.add(tuple(new_state))
+                        queue.append((new_state, path + [current_state]))
+
+                if i < len(current_state) - 1 and current_state[i + 1] != '.':
+                    new_state = current_state[:]
+                    new_state[i], new_state[i + 1] = new_state[i + 1], new_state[i]
+                    if tuple(new_state) not in visited:
+                        visited.add(tuple(new_state))
+                        queue.append((new_state, path + [current_state]))
+
+                if i < len(current_state) - 2 and current_state[i + 1] != '.' and current_state[i + 2] != '.':
+                    new_state = current_state[:]
+                    new_state[i], new_state[i + 2] = new_state[i + 2], new_state[i]
+                    if tuple(new_state) not in visited:
+                        visited.add(tuple(new_state))
+                        queue.append((new_state, path + [current_state]))
+
     return None
 
-def dfs():
-    start = State(3, 3, 0, 0, 1)
-    stack = [(start, [])]
-    explored = set()
-    while stack:
-        current_state, path = stack.pop()
-        if current_state.is_goal():
-            return path + [current_state]
-        explored.add(current_state)
-        for successor in get_successors(current_state):
-            if successor not in explored:
-                stack.append((successor, path + [current_state]))
+# DFS for Rabbit Leap
+def dfs_rabbit_leap(current_state, goal_state, visited):
+    if current_state == goal_state:
+        return [current_state]
+    
+    visited.add(tuple(current_state))
+    
+    for i in range(len(current_state)):
+        if current_state[i] == '.':
+            if i > 0 and current_state[i - 1] != '.':
+                new_state = current_state[:]
+                new_state[i], new_state[i - 1] = new_state[i - 1], new_state[i]
+                if tuple(new_state) not in visited:
+                    result = dfs_rabbit_leap(new_state, goal_state, visited)
+                    if result is not None:
+                        return [current_state] + result
+
+            if i > 1 and current_state[i - 1] != '.' and current_state[i - 2] != '.':
+                new_state = current_state[:]
+                new_state[i], new_state[i - 2] = new_state[i - 2], new_state[i]
+                if tuple(new_state) not in visited:
+                    result = dfs_rabbit_leap(new_state, goal_state, visited)
+                    if result is not None:
+                        return [current_state] + result
+
+            if i < len(current_state) - 1 and current_state[i + 1] != '.':
+                new_state = current_state[:]
+                new_state[i], new_state[i + 1] = new_state[i + 1], new_state[i]
+                if tuple(new_state) not in visited:
+                    result = dfs_rabbit_leap(new_state, goal_state, visited)
+                    if result is not None:
+                        return [current_state] + result
+
+            if i < len(current_state) - 2 and current_state[i + 1] != '.' and current_state[i + 2] != '.':
+                new_state = current_state[:]
+                new_state[i], new_state[i + 2] = new_state[i + 2], new_state[i]
+                if tuple(new_state) not in visited:
+                    result = dfs_rabbit_leap(new_state, goal_state, visited)
+                    if result is not None:
+                        return [current_state] + result
+    
     return None
 
-def print_solution(solution):
-    if solution:
-        print("Solution found:")
-        for step in solution:
-            print(step)
+def solve_rabbit_leap_dfs(initial_state, goal_state):
+    visited = set()
+    result = dfs_rabbit_leap(initial_state, goal_state, visited)
+    if result:
+        return result
     else:
-        print("No solution found.")
+        return "No solution found."
 
-print("BFS Solution:")
-bfs_solution = bfs()
-print_solution(bfs_solution)
+# Test BFS and DFS for Rabbit Leap
+initial_state = ['E', 'E', 'E', '.', 'W', 'W', 'W']
+goal_state = ['W', 'W', 'W', '.', 'E', 'E', 'E']
 
-print("\nDFS Solution:")
-dfs_solution = dfs()
-print_solution(dfs_solution)
+# BFS Solution
+print("BFS Solution for Rabbit Leap:")
+bfs_solution = bfs_rabbit_leap(initial_state, goal_state)
+if bfs_solution:
+    for step in bfs_solution:
+        print(step)
+else:
+    print("No solution found.")
+
+# DFS Solution
+print("\nDFS Solution for Rabbit Leap:")
+dfs_solution = solve_rabbit_leap_dfs(initial_state, goal_state)
+if dfs_solution != "No solution found.":
+    for state in dfs_solution:
+        print(state)
+else:
+    print(dfs_solution)
